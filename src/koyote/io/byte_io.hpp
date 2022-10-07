94f115ee209126fd25ed0da5ed9a6b6e8685dfc1
Gabriel Lugo <g.alexander.lugo@gmail.com>
2022-10-06 23:53:31 -0400

    Update byte_io.hpp

--- src/koyote/io/byte_io.hpp
+++ src/koyote/io/byte_io.hpp
@@ -10,11 +10,11 @@
 #include <optional>
 
 namespace fx::io {
-  [[nodiscard]] inline auto read_bytes(const std::filesystem::path& in_path) -> std::optional<std::vector<uint8_t>>
+  [[nodiscard]] inline auto read_bytes(const std::filesystem::path& in_path) -> std::optional<std::vector<byte>>
   {
     if (std::ifstream in{ in_path, std::ios::binary }; in.is_open()) {
       const std::streamsize size{ static_cast<std::streamsize>(file_size(in_path)) };
-      std::vector<uint8_t> in_byte_buffer(size);
+      std::vector<byte> in_byte_buffer(size);
       in.read(reinterpret_cast<char*>(in_byte_buffer.data()), size);
     
       return in_byte_buffer;
@@ -23,13 +23,39 @@ namespace fx::io {
     return std::nullopt;
   }
   
-  inline auto write_bytes(const std::filesystem::path& out_path, const std::vector<uint8_t>& bytes) -> bool
+  [[nodiscard]] static auto read_words(const std::filesystem::path& in_path) -> std::optional<std::vector<word>>
+  {
+    if (auto bytes{ read_bytes(in_path) }; bytes.has_value()) {
+      const std::streamsize code_word_count{ static_cast<std::streamsize>(bytes.value().size() / 4) };
+      const auto array_start{ reinterpret_cast<u32*>(bytes.value().data()) };
+      std::vector<word> word_buffer{ array_start, array_start + code_word_count };
+      
+      return word_buffer;
+    }
+    
+    return std::nullopt;
+  }
+  
+  inline auto write_bytes(const std::filesystem::path& out_path, const std::vector<byte>& bytes) -> bool
   {
     if (std::ofstream out{ out_path, std::ios::binary }; out.is_open()) {
       out.write(reinterpret_cast<const char*>(bytes.data()), static_cast<std::streamsize>(bytes.size()));
+      
       return true;
     }
   
+    return false;
+  }
+  
+  inline auto write_words(const std::filesystem::path& out_path, const std::vector<word>& words) -> bool
+  {
+    if (std::ofstream out{ out_path, std::ios::binary }; out.is_open()) {
+      const std::streamsize byte_count{ static_cast<std::streamsize>(words.size() * 4) };
+      out.write(reinterpret_cast<const char*>(words.data()), byte_count);
+      
+      return true;
+    }
+    
     return false;
   }
 } // fx // io
