aad129d76dc6cf00f529bc5566a487279814357c
Gabriel Lugo <g.alexander.lugo@gmail.com>
2022-10-14 03:29:17 -0400

    Update macros.hpp

--- src/koyote/core/macros.hpp
+++ src/koyote/core/macros.hpp
@@ -13,25 +13,49 @@
 #define FOXY_STRINGIFY(x) #x
 #define FOXY_LAMBDA_INS(fn, instance) [objPtr = instance](auto&&... args) { return objPtr->fn(std::forward<decltype(args)>(args)...); }
 #define FOXY_LAMBDA(fn) FOXY_LAMBDA_INS(fn, this)
+#define FOXY_ENUMERATE(x) std::views::zip(std::views::iota(0, static_cast<i32>(x.size())), x)
+// for (std::array arr{ 1, 2, 3, 4, 5 }; const auto & [index, element]: FOXY_ENUMERATE(arr)) {
+//
+// }
 
-#ifdef __clang__
-  // CLANG ENABLE/DISABLE WARNING DEFINITION
-  #define FOXY_DISABLE_WARNINGS() _Pragma("clang diagnostic push") \
-	  _Pragma("clang diagnostic ignored \"-Wall\"")                \
-		  _Pragma("clang diagnostic ignored \"-Wextra\"")            \
-		    _Pragma("clang diagnostic ignored \"-Wtautological-compare\"")
-  #define FOXY_ENABLE_WARNINGS() _Pragma("clang diagnostic pop")
-#elif defined(__GNUC__) or defined(__GNUG__)
-  // GCC ENABLE/DISABLE WARNING DEFINITION
-  #define FOXY_DISABLE_WARNINGS() _Pragma("GCC diagnostic push") \
-    _Pragma("GCC diagnostic ignored \"-Wall\"")                \
-		  _Pragma("clang diagnostic ignored \"-Wextra\"")          \
-		    _Pragma("clang diagnostic ignored \"-Wtautological-compare\"")
-  #define FOXY_ENABLE_WARNINGS() _Pragma("GCC diagnostic pop")
-#elif defined(_MSC_VER)
-  // MSVC ENABLE/DISABLE WARNING DEFINITION
-  #define FOXY_DISABLE_WARNINGS() __pragma(warning(push, 0))
-  #define FOXY_ENABLE_WARNINGS() __pragma(warning(pop))
+#if defined(_MSC_VER)
+#define FOXY_DISABLE_WARNINGS __pragma(warning( push, 1 ))
+#define FOXY_ENABLE_WARNINGS  __pragma(warning( pop ))
+
+#define FOXY_DISABLE_WARNING_PUSH           __pragma(warning( push ))
+#define FOXY_DISABLE_WARNING_POP            __pragma(warning( pop )) 
+#define FOXY_DISABLE_WARNING(warningNumber) __pragma(warning( disable : warningNumber ))
+
+#define FOXY_DISABLE_WARNING_UNINITIALIZED_VARIABLE FOXY_DISABLE_WARNING(26495)
+// #define FOXY_DISABLE_WARNING_UNREFERENCED_FORMAL_PARAMETER    FOXY_DISABLE_WARNING(4100)
+// other warnings you want to deactivate...
+#elif defined(__GNUC__) or defined(__clang__)
+#define FOXY_DO_PRAGMA(X) _Pragma(#X)
+
+#define FOXY_DISABLE_WARNINGS \
+  FOXY_DO_PRAGMA(GCC diagnostic push) \
+  FOXY_DO_PRAGMA(GCC diagnostic ignored "-Wall") \
+  FOXY_DO_PRAGMA(clang diagnostic ignored "-Wextra") \
+  FOXY_DO_PRAGMA(clang diagnostic ignored "-Wtautological-compare")
+#define FOXY_ENABLE_WARNINGS  __pragma(warning( pop ))
+
+#define FOXY_DISABLE_WARNING_PUSH         FOXY_DO_PRAGMA(GCC diagnostic push)
+#define FOXY_DISABLE_WARNING_POP          FOXY_DO_PRAGMA(GCC diagnostic pop) 
+#define FOXY_DISABLE_WARNING(warningName) FOXY_DO_PRAGMA(GCC diagnostic ignored #warningName)
+
+#define FOXY_DISABLE_WARNING_UNINITIALIZED_VARIABLE FOXY_DISABLE_WARNING(-Wuninitialized)
+// #define FOXY_DISABLE_WARNING_UNREFERENCED_FORMAL_PARAMETER    FOXY_DISABLE_WARNING(-Wunused-parameter)
+// other warnings you want to deactivate... 
+#else
+#define FOXY_DISABLE_WARNINGS
+#define FOXY_ENABLE_WARNINGS 
+
+#define FOXY_DISABLE_WARNING_PUSH
+#define FOXY_DISABLE_WARNING_POP
+
+#define FOXY_DISABLE_WARNING_UNINITIALIZED_VARIABLE
+// #define FOXY_DISABLE_WARNING_UNREFERENCED_FORMAL_PARAMETER
+// other warnings you want to deactivate... 
 #endif
 
 #if defined(_WIN32) and not defined(FOXY_DEBUG_MODE)
