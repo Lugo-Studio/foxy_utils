a593e8d8f8153b39db5b79f71ec959e9a2ac951b
Gabriel Lugo <g.alexander.lugo@gmail.com>
2022-10-16 07:07:05 -0400

    Alignment

--- src/koyote/core/std.hpp
+++ src/koyote/core/std.hpp
@@ -6,14 +6,14 @@
 namespace fx {
   using byte = std::uint8_t;
   using word = std::uint32_t;
-  using i8 = std::int8_t;
-  using u8 = std::uint8_t;
-  using i16 = std::int16_t;
-  using u16 = std::uint16_t;
-  using i32 = std::int32_t;
-  using u32 = std::uint32_t;
-  using i64 = std::int64_t;
-  using u64 = std::uint64_t;
+  using i8   = std::int8_t;
+  using u8   = std::uint8_t;
+  using i16  = std::int16_t;
+  using u16  = std::uint16_t;
+  using i32  = std::int32_t;
+  using u32  = std::uint32_t;
+  using i64  = std::int64_t;
+  using u64  = std::uint64_t;
 
   struct uuid: public uuids::uuid {
     uuid(): uuids::uuid{ generate() } {}
